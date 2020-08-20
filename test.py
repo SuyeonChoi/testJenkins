b70c2ef0683a9550052b568884ebef7ed857e23b
SuyeonChoi <tndus970120@gmail.com>
2020-08-20 17:03:07 +0900

    add pytesT

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
-print("Hello World!")
-print("test connections")
-print("test connections")
+# import sys
+# input = sys.stdin.readline
+
+def solution(n, arr):
+    result = [-1] * n
+    stack = []
+    for i in range(n - 1, -1, -1):
+        while stack and arr[stack[-1]] <= arr[i]:
+            stack.pop()
+        if stack:
+            result[i] = arr[stack[-1]]
+        stack.append(i)
+
+    # print(" ".join(list(map(str, result))))
+    return list(map(int, result))
+
+
+# N = int(input())
+# sequence = list(map(int, input().split()))
+# result = solution(N, sequence)
+# print(result)
+
+
+def test_simple_test():
+    assert solution(4, [3, 5, 2, 7]) == [5, 7, 8, -1]
+
